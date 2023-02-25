bd305811348cce142e1c339664f47373549fa813
PMBytner <pawel.m.bytner@gmail.com>
2023-02-25 15:46:21 +0100

    Podstawy interfejsu INotifyPropertyChanged

--- NET_PR3_1_Z2/Osoba.cs
+++ NET_PR3_1_Z2/Osoba.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NET_PR3_1_Z2;
-internal class Osoba
+internal class Osoba : INotifyPropertyChanged
 {
-	public string Imię { get; set; } = "Nemo";
+	public event PropertyChangedEventHandler? PropertyChanged;
+	void OnPropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(
+			this,
+			new PropertyChangedEventArgs(propertyName)
+			);
+	}
+
+	private string imię;
+	private string nazwisko;
+
+	public string Imię {
+		get => imię;
+		set {
+			imię = value;
+			OnPropertyChanged("Imię");
+			OnPropertyChanged("ImięNazwisko");
+		}
+	}
+	public string Nazwisko {
+		get => nazwisko;
+		set
+		{
+			nazwisko = value;
+			OnPropertyChanged("Nazwisko");
+			OnPropertyChanged("ImięNazwisko");
+		}
+	}
+	public string ImięNazwisko => $"{imię} {nazwisko}";
+
 }
